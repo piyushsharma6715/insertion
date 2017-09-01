58c9fe77ef736fd8e1208ee3cc4dd4b7f0619427
PIYUSH SHARMA <piyushsharma6715@gmail.com>
2017-09-01 20:53:27 +0530

    Add files via upload

--- p1.cpp
+++ p1.cpp
@@ -4,37 +4,61 @@
 #include<sstream>
 #include<string>
 using namespace std;
-void insertion(int A[],int n,string s,int choice){
-	if(choice==1){
-		int i,j,key;
-			for(j=1;j<n;j++){
-				key=A[j];
-				i=j-1;
-				while(i>-1&&A[i]>key){
-					A[i+1]=A[i];
-					i--;
-				}
-				A[i+1]=key;
-			}
+void insertion(int A[][2],int n){
+	ofstream a ("asc.txt");
+	ofstream b("desc.txt");
+	int i,j,key,key1;
+	for(j=1;j<n;j++){
+		key=A[j][0];
+		key1=A[j][1];
+		i=j-1;
+		while(i>-1&&A[i][0]>key){
+			A[i+1][0]=A[i][0];
+			A[i+1][1]=A[i][1];
+			i--;
 		}
+		A[i+1][0]=key;
+		A[i+1][1]=key1;
+	}
 	for(int i=0;i<n;i++){
-		cout<<A[i]<<"\n";
-	}	
+		a<<A[i][0]<<"\n";
+	}
+	a.close();
+	for(int i=n-1;i>=0;i--){
+		b<<A[i][0]<<"\n";
+	}
+	b.close();
+	ofstream c("priority.txt");
+	for(j=1;j<n;j++){
+		key=A[j][0];
+		key1=A[j][1];
+		i=j-1;
+		while(i>-1&&A[i][1]>key1){
+			A[i+1][1]=A[i][1];
+			A[i+1][0]=A[i][0];
+			i--;
+		}
+		A[i+1][0]=key;
+		A[i+1][1]=key1;
+	}
+	for(int i=0;i<n;i++){
+		c<<A[i][0]<<"\n";
+	}
+	c.close();
 }
 int main(){
 	std::ifstream fp ("num.txt");
 	std::string s;
-	int A[100];
+	int A[100][2];
 	int t=0;
 	while(std::getline(fp,s)){
-		std::istringstream l(s);
-		if(!(l>>A[t])){
+		std::istringstream l(s);     //convert string to int 
+		if(!(l>>A[t][0]>>A[t][1])){
 			break;
 		}
-	cout<<"Number"<<A[t]<<"\n";
 	t++;	
 	}
 
-	insertion(A,t,"HI",1);	
+	insertion(A,t);	
 	return 0;
 }
